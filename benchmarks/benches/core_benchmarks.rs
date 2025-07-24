e9f290f0d1e8d1ab1f2a7dadc95f63f8ff15fa73
vivek pradhan <vivekpradhan.91@gmail.com>
2025-07-24 09:45:51 +0530

    Update rustsec audit action version

--- benchmarks/benches/core_benchmarks.rs
+++ benchmarks/benches/core_benchmarks.rs
@@ -1,12 +1,9 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use rush::greet;
+use std::hint::black_box;
 
 fn benchmark_function(c: &mut Criterion) {
-    c.bench_function("greet", |b| {
-        b.iter(|| {
-            black_box(greet())
-        })
-    });
+    c.bench_function("greet", |b| b.iter(|| black_box(greet())));
 }
 
 criterion_group!(benches, benchmark_function);
